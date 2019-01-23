24026e87d49f7eff37eb86d06e4c26fa0a422317
Tyrell1052 <tyrell1052@gmail.com>
2019-01-22 23:24:56 -0500

    initial commit

---
+++ src/driver.java
@@ -0,0 +1,9 @@
+public class driver {
+    public static void main (String[] args)throws Exception {
+        String[] Driver = new String[200];
+        int count;
+
+
+
+    } // end main()
+} // end driver
---
+++ src/readLines.java
@@ -0,0 +1,13 @@
+public class readLines {
+
+    public static int readLines(String[] lines){
+        int count = 0; //Number of array elements
+
+        // Create a File class
+        
+
+
+    } // end readLines
+
+
+}//end readLines
